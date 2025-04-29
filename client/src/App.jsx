1da123065f33f8e0df10dc687a4b28cf2a51ee2f
Tharaka Madushanka <tharaka.tmw7@gmail.com>
2025-04-29 21:48:58 +0530

    Commit All

---
+++ client/src/App.jsx
@@ -0,0 +1,83 @@
+import { Outlet, useLocation } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Toaster } from 'react-hot-toast';
+import { useEffect, useCallback } from 'react';
+import fetchUserDetails from './utils/fetchUserDetails';
+import { setUserDetails } from './store/userSlice';
+import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/productSlice';
+import { useDispatch } from 'react-redux';
+import Axios from './utils/Axios';
+import SummaryApi from './common/SummaryApi';
+import GlobalProvider from './provider/GlobalProvider';
+import CartMobileLink from './components/CartMobile';
+
+function App() {
+  const dispatch = useDispatch()
+  const location = useLocation()
+  
+
+  const fetchUser = useCallback(async()=>{
+      const userData = await fetchUserDetails()
+      dispatch(setUserDetails(userData.data))
+  }, [dispatch])
+
+  const fetchCategory = useCallback(async()=>{
+    try {
+        dispatch(setLoadingCategory(true))
+        const response = await Axios({
+            ...SummaryApi.getCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        console.error('Error fetching subcategories:', error);
+    } finally {
+      dispatch(setLoadingCategory(false))
+    }
+  }, [dispatch])
+
+  const fetchSubCategory = useCallback(async()=>{
+    try {
+        const response = await Axios({
+            ...SummaryApi.getSubCategory
+        })
+        const { data : responseData } = response
+
+        if(responseData.success){
+           dispatch(setAllSubCategory(responseData.data.sort((a, b) => a.name.localeCompare(b.name)))) 
+        }
+    } catch (error) {
+        console.error('Error fetching subcategories:', error);
+    }
+  }, [dispatch])
+
+  useEffect(()=>{
+    fetchUser()
+    fetchCategory()
+    fetchSubCategory()
+    // fetchCartItem()
+  }, [fetchUser, fetchCategory, fetchSubCategory])
+
+  return (
+    <GlobalProvider> 
+      <Header/>
+      <main className='min-h-[78vh]'>
+          <Outlet/>
+      </main>
+      <Footer/>
+      <Toaster/>
+      {
+        location.pathname !== '/checkout' && (
+          <CartMobileLink/>
+        )
+      }
+    </GlobalProvider>
+  )
+}
+
+export default App
