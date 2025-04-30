7ecc89654fc901f396b6ea391d0c647f9f3bb616
Tharaka Madushanka <tharaka.tmw7@gmail.com>
2025-04-30 23:51:29 +0530

    12 before 9 commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 const auth = async(request,response,next)=>{
     try {
         // const token = request.cookies.accessToken || request?.headers?.authorization?.split(" ")[1]
-       
+
         // if(!token){
         //     return response.status(401).json({
         //         message : "Provide token"
