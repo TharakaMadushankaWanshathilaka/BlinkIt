49c3ed0c5f6561f926709ad89dd68da7e192e282
Tharaka Madushanka <tharaka.tmw7@gmail.com>
2025-05-01 12:17:40 +0530

    change indian rupees to LKR

--- client/src/utils/DisplayPriceInRupees.js
+++ client/src/utils/DisplayPriceInRupees.js
@@ -1,6 +1,6 @@
 export const DisplayPriceInRupees = (price)=>{
-    return new Intl.NumberFormat('en-IN',{
-        style : 'currency',
-        currency : 'INR'
-    }).format(price)
+    return new Intl.NumberFormat('en-IN', {
+        style: 'currency',
+        currency: 'LKR' // LKR stands for Sri Lankan Rupee
+    }).format(price);
 }
